fe70830b37a958a9b33075d7d87fc57a0dc7d71c
WpythoN <45458018+WpythonW@users.noreply.github.com>
2024-09-26 13:26:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = "7344413303:AAGJBHuyPfx0XWRAH2BxReZzwNNYK81cjOo"
-CHANNEL_INVITE_LINK = "https://t.me/+87pn7K_EqLw1NmZi"
-CHANNEL_NAME = "Yummy Linea Бизнес"
+TOKEN = ""
+CHANNEL_INVITE_LINK = ""
+CHANNEL_NAME = ""
 
 DB_HOST = 'localhost'
 DB_PORT = '5432'
